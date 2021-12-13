797ffd94f3299b578c3814fd4c1b1e344dfb5880
Kazi Salith Ur Rahman <salith.rahman107@gmail.com>
2021-12-13 21:25:08 +0600

    completed the todolist final

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todoUL = document.getElementById("todos");
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -15,19 +20,37 @@ function addTodo(todo) {
 
   if (todoText) {
     const todoEl = document.createElement("li"); // create 'li' element
-    // if (todo && todo.completed) {
-    //   todoEl.classList.add("completed");
-    // }
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
     todoEl.innerText = todoText;
-    todoEl.addEventListener("click", () =>
-      todoEl.classList.toggle("completed")
-    );
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      updateLS();
+    });
     todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
       todoEl.remove();
+      updateLS();
     });
 
     todoUL.appendChild(todoEl);
     input.value = "";
+
+    updateLS();
   }
+}
+
+function updateLS() {
+  todoEl = document.querySelectorAll("li");
+  const todos = [];
+
+  todoEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
