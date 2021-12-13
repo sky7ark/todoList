b5ea3ae21534837d9a87a967a86d8158965958e1
Kazi Salith Ur Rahman <salith.rahman107@gmail.com>
2021-12-13 19:37:31 +0600

    updated

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ form.addEventListener("submit", (e) => {
 });
 
 function addTodo(todo) {
-  let todoText = input.value;
+  let todoText = input.value; //input the value and store it in todoText
   if (todo) {
-    todoText = todo.text;
+    todoText = todo.text; // if todo exists then store it in todoText
   }
 
   if (todoText) {
-    const todoEl = document.createElement("li");
-    if (todo && todo.completed) {
-      todoEl.classList.add("completed");
-    }
+    const todoEl = document.createElement("li"); // create 'li' element
+    // if (todo && todo.completed) {
+    //   todoEl.classList.add("completed");
+    // }
     todoEl.innerText = todoText;
     todoEl.addEventListener("click", () =>
       todoEl.classList.toggle("completed")
